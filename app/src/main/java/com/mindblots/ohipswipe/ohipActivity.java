2b5adb793a0edf3d9850df781074aebe82417451
divyesh kokani <divyeshkonkani@gmail.com>
2014-07-05 15:00:41 -0400

    Merge branch 'master' of https://github.com/divyesh/OHIPSwipe
    Conflicts:
    app/src/main/java/com/mindblots/ohipswipe/ohipActivity.java

--- app/src/main/java/com/mindblots/ohipswipe/ohipActivity.java
+++ app/src/main/java/com/mindblots/ohipswipe/ohipActivity.java
@@ -4,12 +4,16 @@ import android.app.Activity;
 import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.text.InputType;
 <<<<<<< HEAD
+import android.util.Log;
 =======
+
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.KeyEvent;
+
 >>>>>>> FETCH_HEAD
 import android.view.Menu;
 import android.view.MenuItem;
@@ -18,26 +22,40 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
-<<<<<<< HEAD
-=======
+
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
->>>>>>> FETCH_HEAD
+
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.protocol.BasicHttpContext;
 import org.apache.http.protocol.HttpContext;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
 
+<<<<<<< HEAD
+=======
+import java.io.BufferedReader;
+>>>>>>> FETCH_HEAD
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -52,6 +70,7 @@ public class ohipActivity extends Activity implements View.OnClickListener/*, Vi
     TextView errortxt;
     Timer timer;
     ClearTask myTask;
+    private final Handler myHandler = new Handler();
     private static final int[] BUTTON_IDS = {
             R.id.one,
             R.id.two,
@@ -96,17 +115,7 @@ public class ohipActivity extends Activity implements View.OnClickListener/*, Vi
         if(ot.length()<10)
         _ohipText.setText(ot+newdigit);
     }
-   /* @Override
-    public boolean onKey(View v, int keyCode, KeyEvent event)
-    {
-        if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
-                (keyCode == KeyEvent.KEYCODE_ENTER))
-        {
-            ReadInput();
-        }
-        // Returning false allows other listeners to react to the press.
-        return false;
-    }*/
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -135,38 +144,44 @@ public class ohipActivity extends Activity implements View.OnClickListener/*, Vi
             errortxt.setText("Valid Health Card");
         }
     }
+
     private void ReadInput()
     {
         HealthCard hc=new HealthCard();
         String ot=_ohipText.getText().toString();
         //ot=ot.replace(" ","");
-        if(ot.length()==10) {
-            String otn= ot.substring(0,4)+" "+ot.substring(4,7)+" "+ot.substring(7,10);
-            _ohipText.setText(otn);
-        }
-        else if(ot.length()>9) {
+       // if(ot.length()==10) {
+            //String otn= ot.substring(0,4)+" "+ot.substring(4,7)+" "+ot.substring(7,10);
+            //_ohipText.setText(otn);
+       // }
+        //if(ot.length()>9)
+        {
 
             try {
+
                 Boolean valid=true;
 
                 if(ot.length()==10)
                 {
                     valid=hc.Validation(ot);
+
                 }else {
                     hc.Read(ot);
                     valid=hc.valid;
                 }
+
                 Error(valid);
             } catch (Exception e) {
                 e.printStackTrace();
             }
-            // new LongRunningGetIO().execute();
+            new HttpAsyncTask().execute(ot);//("%B6100542599591129^KOKANI/DIVYESHKUMA C     ^1408799119820826JHDIVYE11073001?;6100542599591129=14087990000100000000?+906100542599591129=0000000000000000000000000000000000000000000000000000000000000000000000000000000000000?");
             tokenInfo.setText("Your token number is");
             timer = new Timer();
             myTask = new ClearTask();
             timer.schedule(myTask, 9000);
         }
     }
+<<<<<<< HEAD
     public void postData(String hci) {
         // Create a new HttpClient and Post Header
         HttpClient httpclient = new DefaultHttpClient();
@@ -182,13 +197,15 @@ public class ohipActivity extends Activity implements View.OnClickListener/*, Vi
             // Execute HTTP Post Request
             HttpResponse response = httpclient.execute(httppost);
             InputStream inputStream = response.getEntity().getContent();
-            Toast.makeText(getBaseContext(),inputStream.toString(),Toast.LENGTH_SHORT);
+            Toast.makeText(getBaseContext(), inputStream.toString(), Toast.LENGTH_SHORT);
         } catch (ClientProtocolException e) {
             // TODO Auto-generated catch block
         } catch (IOException e) {
             // TODO Auto-generated catch block
         }
     }
+=======
+>>>>>>> FETCH_HEAD
 
     @Override
     public void onClick(View v) {
@@ -233,6 +250,92 @@ public class ohipActivity extends Activity implements View.OnClickListener/*, Vi
 
        }
     }
+    private class HttpAsyncTask extends AsyncTask<String, Integer, Double>{
+
+        @Override
+        protected Double doInBackground(String... params) {
+            // TODO Auto-generated method stub
+            //Toast.makeText(getApplicationContext(), params[0], Toast.LENGTH_LONG).show();
+            Log.i("OHIP Entered",params[0]);
+            final String json=postData(params[0]);
+
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        webToken w= null;
+                        try {
+                            w = toToken(json);
+                         ohipActivity.this.tokenNum.setText(w.Token);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+
+                    }
+                });
+
+            Log.i("OHIP SENT",params[0]);
+            return null;
+        }
+
+        protected void onPostExecute(Double result){
+
+            Toast.makeText(getApplicationContext(), "Contacting Server", Toast.LENGTH_LONG).show();
+        }
+
+
+        public String  postData(String valueIWantToSend) {
+            // Create a new HttpClient and Post Header
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpPost httppost = new HttpPost("http://dymo.herokuapp.com/tokens.json");
+
+            try {
+                // Add your data
+                JSONObject data = new JSONObject();
+                data.put("healthcard",valueIWantToSend);
+                StringEntity se = new StringEntity(data.toString());
+
+                //sets the post request as the resulting string
+                httppost.setEntity(se);
+                //sets a request header so the page receving the request
+                //will know what to do with it
+                httppost.setHeader("Accept", "application/json");
+                httppost.setHeader("Content-type", "application/json");
+
+                // Execute HTTP Post Request
+                HttpResponse response = httpclient.execute(httppost);
+                Header[] headers = response.getAllHeaders();
+                for (Header header : headers) {
+                    Log.i("OHIP Response", "Key : " + header.getName()+ " ,Value : " + header.getValue());
+                }
+                BufferedReader reader = new BufferedReader(new InputStreamReader(response.getEntity().getContent(), "UTF-8"));
+                String json = reader.readLine();
+
+                return json;
+            } catch (ClientProtocolException e) {
+                Log.i("OHIP Response ERROR",e.toString());
+            } catch (IOException e) {
+                Log.i("OHIP Response ERROR",e.toString());
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+        public  webToken toToken(String json) throws JSONException {
+
+                JSONObject jo =new JSONObject(json);
+                String id = jo.getString("id");
+                String token = jo.getString("no");
+                String state = jo.getString("state");
+                String hc = jo.getJSONObject("patient").getString("healthnumber");
+
+                if (!state.equals("completed")) {
+                    webToken w = new webToken(id, token, hc, state, "");
+
+                    return w;
+                }
+return null;
+        }
+    }
     class ClearTask extends TimerTask {
 
         @Override
@@ -249,48 +352,5 @@ public class ohipActivity extends Activity implements View.OnClickListener/*, Vi
                 }});
         }
 
-    }
-    private class LongRunningGetIO extends AsyncTask<Void, Void, String> {
-
-        protected String getASCIIContentFromEntity(HttpEntity entity) throws Exception {
-            InputStream in = entity.getContent();
-            StringBuffer out = new StringBuffer();
-            int n = 1;
-            while (n>0) {
-                byte[] b = new byte[4096];
-                n =  in.read(b);
-                if (n>0) out.append(new String(b, 0, n));
-            }
-            return out.toString();
-        }
-
-        @Override
-        protected String doInBackground(Void... params) {
-
-            // Send Ohip text
-            String ot=_ohipText.getText().toString();
-
-            HttpClient httpClient =new DefaultHttpClient();
-            HttpContext localContext = new BasicHttpContext();
-            HttpPost httpPost = new HttpPost("http://localhost:3000/NewPatient/"+ot);
-            String text = null;
-            try {
-                HttpResponse response = httpClient.execute(httpPost, localContext);
-                HttpEntity entity = response.getEntity();
-                text = getASCIIContentFromEntity(entity);
-            } catch (Exception e) {
-                return e.getLocalizedMessage();
-            }
-            return text;
-        }
-
-        protected void onPostExecute(String results) {
-            if (results!=null) {
-                tokenInfo.setText("Your token number is");
-                String rslt[]=results.split("|");
-                tokenNum.setText(rslt[0]);
-                waittime.setText(waittimetxt+" "+ rslt[1]);
-            }
-        }
     }
 }
